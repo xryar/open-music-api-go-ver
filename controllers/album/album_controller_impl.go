4a8554077cb126b31ad9452a7b2ba4742c308a5b
Arya Rizki Andaru <aryarizki3045@gmail.com>
2026-01-26 12:57:40 +0700

    implement album controller  contracts

--- controllers/album/album_controller_impl.go
+++ controllers/album/album_controller_impl.go
@@ -2,7 +2,11 @@ package controllers
 
 import (
 	"net/http"
+	"open-music-go/helper"
+	"open-music-go/model/web"
+	req "open-music-go/model/web/album"
 	services "open-music-go/services/album"
+	"strconv"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -16,21 +20,76 @@ func NewAlbumService(service services.AlbumService) *AlbumControllerImpl {
 }
 
 func (ac *AlbumControllerImpl) CreateAlbum(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	albumCreateRequest := req.CreateAlbumRequest{}
+	helper.ReadFromRequestBody(r, &albumCreateRequest)
 
+	albumResponse := ac.service.CreateAlbum(r.Context(), albumCreateRequest)
+	webResponse := web.WebResponse{
+		Code:   201,
+		Status: "OK",
+		Data:   albumResponse,
+	}
+
+	helper.WriteToResponseBody(w, webResponse)
 }
 
 func (ac *AlbumControllerImpl) UpdateAlbum(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	albumUpdateRequest := req.UpdateAlbumRequest{}
+	helper.ReadFromRequestBody(r, &albumUpdateRequest)
 
+	albumId := p.ByName("albumId")
+	id, err := strconv.Atoi(albumId)
+	helper.PanicIfError(err)
+
+	albumUpdateRequest.Id = id
+
+	albumResponse := ac.service.UpdateAlbum(r.Context(), albumUpdateRequest)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   albumResponse,
+	}
+
+	helper.WriteToResponseBody(w, webResponse)
 }
 
 func (ac *AlbumControllerImpl) DeleteAlbum(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	albumId := p.ByName("albumId")
+	id, err := strconv.Atoi(albumId)
+	helper.PanicIfError(err)
 
+	ac.service.DeleteAlbum(r.Context(), id)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+	}
+
+	helper.WriteToResponseBody(w, webResponse)
 }
 
 func (ac *AlbumControllerImpl) FindByAlbumId(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	albumId := p.ByName("albumId")
+	id, err := strconv.Atoi(albumId)
+	helper.PanicIfError(err)
+
+	albumResponse := ac.service.FindByAlbumId(r.Context(), id)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   albumResponse,
+	}
+
+	helper.WriteToResponseBody(w, webResponse)
 
 }
 
 func (ac *AlbumControllerImpl) FindAllAlbum(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	albumResponses := ac.service.FindAllAlbum(r.Context())
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   albumResponses,
+	}
 
+	helper.WriteToResponseBody(w, webResponse)
 }
