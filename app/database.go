3b38fc60977d6c26038c2a37f1a5eac74d82e9cd
Arya Rizki Andaru <aryarizki3045@gmail.com>
2026-01-26 09:35:29 +0700

    setup database

---
+++ app/database.go
@@ -0,0 +1,19 @@
+package app
+
+import (
+	"database/sql"
+	"open-music-go/helper"
+	"time"
+)
+
+func NewDB() *sql.DB {
+	db, err := sql.Open("mysql", "root:acumalaka@tcp(localhost:3306)/open-music-go")
+	helper.PanicIfError(err)
+
+	db.SetMaxIdleConns(5)
+	db.SetMaxOpenConns(10)
+	db.SetConnMaxLifetime(60 * time.Minute)
+	db.SetConnMaxIdleTime(10 * time.Minute)
+
+	return db
+}
