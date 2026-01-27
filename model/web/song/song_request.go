90e15754073fd21adac69e15bff90e48bb9de8d6
Arya Rizki Andaru <aryarizki3045@gmail.com>
2026-01-27 09:56:34 +0700

    refactor: rename CreateSong and UpdateSong types to CreateSongRequest and UpdateSongRequest for clarity

--- model/web/song/song_request.go
+++ model/web/song/song_request.go
@@ -1,6 +1,6 @@
 package web
 
-type CreateSong struct {
+type CreateSongRequest struct {
 	Title     string `json:"title" validate:"required,min=1,max=255"`
 	Year      int    `json:"year" validate:"required,min=1,max=255"`
 	Genre     string `json:"genre" validate:"required,min=1,max=255"`
@@ -9,7 +9,7 @@ type CreateSong struct {
 	AlbumId   int    `json:"albumId" validate:"min=1,max=255"`
 }
 
-type UpdateSong struct {
+type UpdateSongRequest struct {
 	Id        int    `json:"id"`
 	Title     string `json:"title" validate:"required,min=1,max=255"`
 	Year      int    `json:"year" validate:"required,min=1,max=255"`
