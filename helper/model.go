661ef56ad94b40b8ae96250a204576bf11184ac9
Arya Rizki Andaru <aryarizki3045@gmail.com>
2026-01-27 10:54:10 +0700

    create helper for song response

--- helper/model.go
+++ helper/model.go
@@ -3,6 +3,7 @@ package helper
 import (
 	"open-music-go/model/domain"
 	web "open-music-go/model/web/album"
+	web2 "open-music-go/model/web/song"
 )
 
 func ToAlbumResponse(album domain.Album) web.AlbumResponse {
@@ -20,4 +21,25 @@ func ToAlbumResponses(albums []domain.Album) []web.AlbumResponse {
 	}
 
 	return albumResponses
+}
+
+func ToSongResponse(song domain.Song) web2.SongResponse {
+	return web2.SongResponse{
+		Id:        song.Id,
+		Title:     song.Title,
+		Year:      song.Year,
+		Performer: song.Performer,
+		Genre:     song.Genre,
+		Duration:  song.Duration,
+		AlbumId:   song.AlbumId,
+	}
+}
+
+func ToSongResponses(songs []domain.Song) []web2.SongResponse {
+	var SongResponses []web2.SongResponse
+	for _, song := range songs {
+		SongResponses = append(SongResponses, ToSongResponse(song))
+	}
+
+	return SongResponses
 }
