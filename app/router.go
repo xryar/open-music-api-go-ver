65649525dfdf0ac2cda8efc53d643cdbf255a85d
Arya Rizki Andaru <aryarizki3045@gmail.com>
2026-01-28 14:40:14 +0700

    refactor: restructure NewRouter function to include song controller and improve routing organization

--- app/router.go
+++ app/router.go
@@ -1,21 +1,38 @@
 package app
 
 import (
-	controllers "open-music-go/controllers/album"
+	albumControllers "open-music-go/controllers/album"
+	songControllers "open-music-go/controllers/song"
 	"open-music-go/exception"
 
 	"github.com/julienschmidt/httprouter"
 )
 
-func NewRouter(albumController controllers.AlbumController) *httprouter.Router {
+func NewRouter(
+	albumController albumControllers.AlbumController,
+	songController songControllers.SongController,
+) *httprouter.Router {
 	router := httprouter.New()
+	albumRouter(router, albumController)
+	songRouter(router, songController)
+
+	router.PanicHandler = exception.ErrorHandler
+
+	return router
+}
+
+func albumRouter(router *httprouter.Router, albumController albumControllers.AlbumController) {
 	router.GET("/api/albums", albumController.FindAllAlbum)
 	router.GET("/api/albums/:albumId", albumController.FindByAlbumId)
 	router.POST("/api/albums", albumController.CreateAlbum)
 	router.PUT("/api/albums/:albumId", albumController.UpdateAlbum)
 	router.DELETE("/api/albums/:albumId", albumController.DeleteAlbum)
+}
 
-	router.PanicHandler = exception.ErrorHandler
-
-	return router
+func songRouter(router *httprouter.Router, songController songControllers.SongController) {
+	router.GET("/api/songs", songController.FindAllSong)
+	router.GET("/api/songs/:songId", songController.FindSongById)
+	router.POST("/api/songs", songController.CreateSong)
+	router.PUT("/api/songs/:songId", songController.UpdateSong)
+	router.DELETE("/api/songs/:songId", songController.DeleteSong)
 }
