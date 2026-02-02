74e3c79750c02110eb85be9804f6be3bb725e8b5
Arya Rizki Andaru <aryarizki3045@gmail.com>
2026-02-02 11:36:33 +0700

    user migration file

---
+++ db/migrations/20260202042628_create_table_users.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    fullname VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+) ENGINE = InnoDB;
