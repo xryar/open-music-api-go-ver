2dd38ea7ebd4fa6fe637dcc803670076b538934f
Arya Rizki Andaru <aryarizki3045@gmail.com>
2026-02-03 10:47:48 +0700

    add user controller and repository to main application

--- main.go
+++ main.go
@@ -6,11 +6,14 @@ import (
 	"open-music-go/app"
 	albumController "open-music-go/controllers/album"
 	songController "open-music-go/controllers/song"
+	userController "open-music-go/controllers/user"
 	"open-music-go/helper"
 	albumRepository "open-music-go/repositories/album"
 	songRepository "open-music-go/repositories/song"
+	userRepository "open-music-go/repositories/user"
 	albumService "open-music-go/services/album"
 	songService "open-music-go/services/song"
+	userService "open-music-go/services/user"
 
 	"github.com/go-playground/validator/v10"
 	_ "github.com/go-sql-driver/mysql"
@@ -21,12 +24,15 @@ func main() {
 	validate := validator.New()
 	albumRepository := albumRepository.NewAlbumRepository()
 	songRepository := songRepository.NewSongRepository()
+	userRepository := userRepository.NewUserRepository()
 	albumService := albumService.NewAlbumService(albumRepository, db, validate)
 	songService := songService.NewSongService(songRepository, db, validate)
+	userService := userService.NewUserService(userRepository, db, validate)
 	albumController := albumController.NewAlbumController(albumService)
 	songController := songController.NewSongController(songService)
+	userController := userController.NewUserController(userService)
 
-	router := app.NewRouter(albumController, songController)
+	router := app.NewRouter(albumController, songController, userController)
 
 	server := http.Server{
 		Addr:    "localhost:3000",
