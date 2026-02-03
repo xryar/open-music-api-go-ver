8dcbcdd840f9a2cb32a29a0bc41ec2cd20bbbd98
Arya Rizki Andaru <aryarizki3045@gmail.com>
2026-02-03 10:46:18 +0700

    delete password in register response

--- model/web/user/user_response.go
+++ model/web/user/user_response.go
@@ -4,7 +4,6 @@ type UserRegisterResponse struct {
 	Id       int    `json:"id"`
 	Fullname string `json:"fullname"`
 	Username string `json:"username"`
-	Password string `json:"password"`
 }
 
 type UserLoginResponse struct {
