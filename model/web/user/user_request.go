e90e131f02797e5a120d2a8e39f5d3862ff7ab6f
Arya Rizki Andaru <aryarizki3045@gmail.com>
2026-02-03 10:46:04 +0700

    fix validation bug in register request

--- model/web/user/user_request.go
+++ model/web/user/user_request.go
@@ -1,7 +1,7 @@
 package web
 
 type UserRegisterRequest struct {
-	Fullname string `json:"fullname" validate:"required,min=4,max255"`
+	Fullname string `json:"fullname" validate:"required,min=4,max=255"`
 	Username string `json:"username" validate:"required,min=4,max=255"`
 	Password string `json:"password" validate:"required,min=6,max=25"`
 }
