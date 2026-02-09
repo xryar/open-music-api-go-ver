78e5fa37eb5b9fba4421960c87da679b99d88098
Arya Rizki Andaru <aryarizki3045@gmail.com>
2026-02-09 11:16:23 +0700

    implement create and delete playlist

--- repositories/playlist/playlist_repository_impl.go
+++ repositories/playlist/playlist_repository_impl.go
@@ -3,6 +3,7 @@ package repositories
 import (
 	"context"
 	"database/sql"
+	"open-music-go/helper"
 	"open-music-go/model/domain"
 )
 
@@ -14,11 +15,21 @@ func NewPlaylistRepository() *PlaylistRepositoryImpl {
 }
 
 func (pr *PlaylistRepositoryImpl) CreatePlaylist(ctx context.Context, tx *sql.Tx, playlist domain.Playlist) domain.Playlist {
+	SQL := "INSERT INTO playlists(name, owner) VALUES (?, ?)"
+	result, err := tx.ExecContext(ctx, SQL, playlist.Name, playlist.Owner)
+	helper.PanicIfError(err)
 
+	id, err := result.LastInsertId()
+	helper.PanicIfError(err)
+
+	playlist.Id = int(id)
+	return playlist
 }
 
 func (pr *PlaylistRepositoryImpl) DeletePlaylist(ctx context.Context, tx *sql.Tx, id int) {
-
+	SQL := "DELETE FROM playlist WHERE id = ?"
+	_, err := tx.ExecContext(ctx, SQL, id)
+	helper.PanicIfError(err)
 }
 
 func (pr *PlaylistRepositoryImpl) FindPlaylistById(ctx context.Context, tx *sql.Tx, id int) []domain.Playlist {
