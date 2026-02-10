b5dade24aa955a9e33be5871756eec7f9ca9fac6
Arya Rizki Andaru <aryarizki3045@gmail.com>
2026-02-10 10:26:37 +0700

    update playlist response

--- model/web/playlist/playlist_response.go
+++ model/web/playlist/playlist_response.go
@@ -3,8 +3,8 @@ package web
 import web "open-music-go/model/web/song"
 
 type PlaylistResponse struct {
-	Id       int                `json:"id"`
-	Name     string             `json:"name"`
-	Username string             `json:"username"`
-	Songs    []web.SongResponse `json:"songs,omitempty"`
+	Id    int                `json:"id"`
+	Name  string             `json:"name"`
+	Owner int                `json:"owner"`
+	Songs []web.SongResponse `json:"songs,omitempty"`
 }
