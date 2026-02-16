5f0203a9a56f40d826bee34c113368ffba2d7068
Arya Rizki Andaru <aryarizki3045@gmail.com>
2026-02-16 11:02:51 +0700

    implement playlist controller

--- controllers/playlist/playlist_controller_impl.go
+++ controllers/playlist/playlist_controller_impl.go
@@ -2,7 +2,11 @@ package controllers
 
 import (
 	"net/http"
+	"open-music-go/helper"
+	"open-music-go/model/web"
+	req "open-music-go/model/web/playlist"
 	services "open-music-go/services/playlist"
+	"strconv"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -16,27 +20,105 @@ func NewPlaylistController(service services.PlaylistService) *PlaylistController
 }
 
 func (pc *PlaylistControllerImpl) CreatePlaylist(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	playlistRequest := req.CreatePlaylistRequest{}
+	helper.ReadFromRequestBody(r, &playlistRequest)
 
+	playlist, err := pc.service.CreatePlaylist(r.Context(), playlistRequest)
+	helper.PanicIfError(err)
+
+	webResponse := web.WebResponse{
+		Code:    201,
+		Status:  "OK",
+		Message: "Success Create Playlist",
+		Data:    playlist,
+	}
+
+	helper.WriteToResponseBody(w, webResponse)
 }
 
 func (pc *PlaylistControllerImpl) AddSongToPlaylist(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	bodyRequest := req.SongToPlaylistRequest{}
+	helper.ReadFromRequestBody(r, &bodyRequest)
 
-}
+	playlistId := p.ByName("playlistId")
+	id, err := strconv.Atoi(playlistId)
+	helper.PanicIfError(err)
 
-func (pc *PlaylistControllerImpl) UpdatePlaylist(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	request := req.PlaylistSongRequest{
+		PlaylistId: id,
+		SongId:     bodyRequest.SongId,
+	}
 
+	err = pc.service.AddSongToPlaylist(r.Context(), request)
+	helper.PanicIfError(err)
+
+	webResponse := web.WebResponse{
+		Code:    201,
+		Status:  "OK",
+		Message: "Success Add Song To Playlist",
+	}
+
+	helper.WriteToResponseBody(w, webResponse)
 }
 
 func (pc *PlaylistControllerImpl) DeletePlaylist(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	playlistId := p.ByName("playlistId")
+	id, err := strconv.Atoi(playlistId)
+	helper.PanicIfError(err)
 
+	err = pc.service.DeletePlaylist(r.Context(), id)
+	helper.PanicIfError(err)
+
+	webResponse := web.WebResponse{
+		Code:    200,
+		Status:  "OK",
+		Message: "Success Delete Playlist",
+	}
+
+	helper.WriteToResponseBody(w, webResponse)
 }
 
 func (pc *PlaylistControllerImpl) DeleteSongInPlaylist(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	bodyRequest := req.SongToPlaylistRequest{}
+	helper.ReadFromRequestBody(r, &bodyRequest)
 
+	playlistId := p.ByName("playlistId")
+	id, err := strconv.Atoi(playlistId)
+	helper.PanicIfError(err)
+
+	request := req.PlaylistSongRequest{
+		PlaylistId: id,
+		SongId:     bodyRequest.SongId,
+	}
+
+	err = pc.service.DeleteSongInPlaylist(r.Context(), request)
+	helper.PanicIfError(err)
+
+	webResponse := web.WebResponse{
+		Code:    200,
+		Status:  "OK",
+		Message: "Song removed from playlist",
+	}
+
+	helper.WriteToResponseBody(w, webResponse)
 }
 
 func (pc *PlaylistControllerImpl) FindByPlaylistId(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	playlistId := p.ByName("playlistId")
+	id, err := strconv.Atoi(playlistId)
+	helper.PanicIfError(err)
 
+	playlist, err := pc.service.FindPlaylistById(r.Context(), id)
+	helper.PanicIfError(err)
+
+	webResponse := web.WebResponse{
+		Code:    200,
+		Status:  "OK",
+		Message: "Success Get Playlist",
+		Data:    playlist,
+	}
+
+	helper.WriteToResponseBody(w, webResponse)
 }
 
 func (pc *PlaylistControllerImpl) FindAllPlaylist(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
