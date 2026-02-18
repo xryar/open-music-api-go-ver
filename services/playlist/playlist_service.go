4af5039969ed3b5f15e699746e353417db27ada9
Arya Rizki Andaru <aryarizki3045@gmail.com>
2026-02-18 09:12:24 +0700

    update playlist service contracts

--- services/playlist/playlist_service.go
+++ services/playlist/playlist_service.go
@@ -9,6 +9,7 @@ type PlaylistService interface {
 	CreatePlaylist(ctx context.Context, request web.CreatePlaylistRequest) (web.PlaylistResponse, error)
 	AddSongToPlaylist(ctx context.Context, request web.PlaylistSongRequest) error
 	DeleteSongInPlaylist(ctx context.Context, request web.PlaylistSongRequest) error
+	FindPlaylistByOwner(ctx context.Context) ([]web.PlaylistResponse, error)
 	DeletePlaylist(ctx context.Context, id int) error
 	FindPlaylistById(ctx context.Context, id int) (web.PlaylistResponse, error)
 	FindAllPlaylists(ctx context.Context) ([]web.PlaylistResponse, error)
