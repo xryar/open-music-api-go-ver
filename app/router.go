ccc74b31890bd9e841718efb065e203c865c3c96
Arya Rizki Andaru <aryarizki3045@gmail.com>
2026-02-18 09:18:22 +0700

    create playlist route

--- app/router.go
+++ app/router.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	albumControllers "open-music-go/controllers/album"
+	playlistControllers "open-music-go/controllers/playlist"
 	songControllers "open-music-go/controllers/song"
 	userControllers "open-music-go/controllers/user"
 	"open-music-go/exception"
@@ -14,11 +15,13 @@ func NewRouter(
 	albumController albumControllers.AlbumController,
 	songController songControllers.SongController,
 	userController userControllers.UserController,
+	playlistController playlistControllers.PlaylistController,
 ) *httprouter.Router {
 	router := httprouter.New()
 	albumRouter(router, albumController)
 	songRouter(router, songController)
 	userRouter(router, userController)
+	playlistRouter(router, playlistController)
 
 	router.PanicHandler = exception.ErrorHandler
 
@@ -26,22 +29,32 @@ func NewRouter(
 }
 
 func albumRouter(router *httprouter.Router, albumController albumControllers.AlbumController) {
-	router.GET("/api/albums", middlewares.AuthMiddleware(albumController.FindAllAlbum))
-	router.GET("/api/albums/:albumId", middlewares.AuthMiddleware(albumController.FindByAlbumId))
-	router.POST("/api/albums", middlewares.AuthMiddleware(albumController.CreateAlbum))
-	router.PUT("/api/albums/:albumId", middlewares.AuthMiddleware(albumController.UpdateAlbum))
-	router.DELETE("/api/albums/:albumId", middlewares.AuthMiddleware(albumController.DeleteAlbum))
+	router.GET("/api/albums", albumController.FindAllAlbum)
+	router.GET("/api/albums/:albumId", albumController.FindByAlbumId)
+	router.POST("/api/albums", albumController.CreateAlbum)
+	router.PUT("/api/albums/:albumId", albumController.UpdateAlbum)
+	router.DELETE("/api/albums/:albumId", albumController.DeleteAlbum)
 }
 
 func songRouter(router *httprouter.Router, songController songControllers.SongController) {
-	router.GET("/api/songs", middlewares.AuthMiddleware(songController.FindAllSong))
-	router.GET("/api/songs/:songId", middlewares.AuthMiddleware(songController.FindSongById))
-	router.POST("/api/songs", middlewares.AuthMiddleware(songController.CreateSong))
-	router.PUT("/api/songs/:songId", middlewares.AuthMiddleware(songController.UpdateSong))
-	router.DELETE("/api/songs/:songId", middlewares.AuthMiddleware(songController.DeleteSong))
+	router.GET("/api/songs", songController.FindAllSong)
+	router.GET("/api/songs/:songId", songController.FindSongById)
+	router.POST("/api/songs", songController.CreateSong)
+	router.PUT("/api/songs/:songId", songController.UpdateSong)
+	router.DELETE("/api/songs/:songId", songController.DeleteSong)
 }
 
 func userRouter(router *httprouter.Router, userController userControllers.UserController) {
 	router.POST("/api/user/register", userController.Register)
 	router.POST("/api/user/login", userController.Login)
+}
+
+func playlistRouter(router *httprouter.Router, playlistController playlistControllers.PlaylistController) {
+	router.POST("/api/playlists", middlewares.AuthMiddleware(playlistController.CreatePlaylist))
+	router.GET("/api/playlists/me", middlewares.AuthMiddleware(playlistController.FindPlaylistByOwner))
+	router.GET("/api/playlists", playlistController.FindAllPlaylist)
+	router.DELETE("/api/playlists/:id", middlewares.AuthMiddleware(playlistController.DeletePlaylist))
+	router.POST("/api/playlists/:id/songs", middlewares.AuthMiddleware(playlistController.AddSongToPlaylist))
+	router.GET("/api/playlists/:id/songs", middlewares.AuthMiddleware(playlistController.FindByPlaylistId))
+	router.DELETE("/api/playlists/:id/songs", middlewares.AuthMiddleware(playlistController.DeleteSongInPlaylist))
 }
