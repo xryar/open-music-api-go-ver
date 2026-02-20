ebb611fe0e610c3e3f2838e2942193d86c31444c
Arya Rizki Andaru <aryarizki3045@gmail.com>
2026-02-20 14:18:28 +0700

    implement activity contracts

---
+++ repositories/playlist_activity/playlist_activity_repository_impl.go
@@ -0,0 +1,60 @@
+package repositories
+
+import (
+	"context"
+	"database/sql"
+	"open-music-go/model/domain"
+	web "open-music-go/model/web/playlist_activity"
+)
+
+type PlaylistActivityRepositoryImpl struct {
+}
+
+func NewPlaylistActivityRepository() *PlaylistActivityRepositoryImpl {
+	return &PlaylistActivityRepositoryImpl{}
+}
+
+func (ar *PlaylistActivityRepositoryImpl) Create(ctx context.Context, tx *sql.Tx, activity domain.PlaylistActivity) error {
+	SQL := "INSERT INTO playlist_song_activities (playlist_id, song_id, user_id, action) VALUES (?, ?, ?, ?)"
+	_, err := tx.ExecContext(ctx, SQL, activity.PlaylistId, activity.SongId, activity.UserId, activity.Action)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ar *PlaylistActivityRepositoryImpl) FindPlaylistById(ctx context.Context, tx *sql.Tx, playlistId int) ([]web.Activity, error) {
+	SQL := `
+	SELECT
+		u.username,
+		s.title,
+		a.action,
+		a.time
+	FROM playlist_song_activities a
+	JOIN users u ON u.id = a.user_id
+	JOIN song s ON s.id = a.song_id
+	WHERE a.playlist_id = ?
+	ORDER BY a.time DESC
+	`
+
+	rows, err := tx.QueryContext(ctx, SQL, playlistId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	activities := []web.Activity{}
+
+	for rows.Next() {
+		activity := web.Activity{}
+		err := rows.Scan(&activity.Username, &activity.SongTitle, &activity.Action, &activity.Time)
+		if err != nil {
+			return nil, err
+		}
+
+		activities = append(activities, activity)
+	}
+
+	return activities, nil
+}
