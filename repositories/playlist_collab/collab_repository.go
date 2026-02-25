61dbce5583a505e4683e3c6d8f59150c6bae4a94
Arya Rizki Andaru <aryarizki3045@gmail.com>
2026-02-25 09:25:58 +0700

    create collab repo contracts

---
+++ repositories/playlist_collab/collab_repository.go
@@ -0,0 +1,14 @@
+package repositories
+
+import (
+	"context"
+	"database/sql"
+	"open-music-go/model/domain"
+)
+
+type PlaylistCollabRepository interface {
+	Add(ctx context.Context, tx *sql.Tx, collaborator domain.PlaylistCollab) error
+	Remove(ctx context.Context, tx *sql.Tx, collaborator domain.PlaylistCollab) error
+	IsCollab(ctx context.Context, tx *sql.Tx, collaborator domain.PlaylistCollab) (bool, error)
+	FindByPlaylistId(ctx context.Context, tx *sql.Tx, playlistId int) ([]int, error)
+}
