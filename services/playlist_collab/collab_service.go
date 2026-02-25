ec9470901b6698504ec251112915d5529e6a0ac1
Arya Rizki Andaru <aryarizki3045@gmail.com>
2026-02-25 11:12:59 +0700

    feat: implement PlaylistCollabService and its methods for managing collaborators

---
+++ services/playlist_collab/collab_service.go
@@ -0,0 +1,12 @@
+package services
+
+import (
+	"context"
+	web "open-music-go/model/web/collab"
+)
+
+type PlaylistCollabService interface {
+	AddCollaborator(ctx context.Context, request web.CollabRequest) error
+	RemoveCollaborator(ctx context.Context, request web.CollabRequest) error
+	GetCollaborators(ctx context.Context, playlistId int) ([]int, error)
+}
