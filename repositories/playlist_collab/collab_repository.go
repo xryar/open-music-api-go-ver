24df0e08c1b50057f808eb1a8dabb3917cbcb1ea
Arya Rizki Andaru <aryarizki3045@gmail.com>
2026-02-26 10:19:10 +0700

    feat: update GetCollaborators method to return domain.User instead of user IDs

--- repositories/playlist_collab/collab_repository.go
+++ repositories/playlist_collab/collab_repository.go
@@ -10,5 +10,5 @@ type PlaylistCollabRepository interface {
 	Add(ctx context.Context, tx *sql.Tx, collaborator domain.PlaylistCollab) error
 	Remove(ctx context.Context, tx *sql.Tx, collaborator domain.PlaylistCollab) error
 	IsCollab(ctx context.Context, tx *sql.Tx, collaborator domain.PlaylistCollab) (bool, error)
-	GetCollaborators(ctx context.Context, tx *sql.Tx, playlistId int) ([]int, error)
+	GetCollaborators(ctx context.Context, tx *sql.Tx, playlistId int) ([]domain.User, error)
 }
